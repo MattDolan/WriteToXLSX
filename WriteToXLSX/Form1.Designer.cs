4acc6d206c8c7b0365406ead61be4dd2e7b88713
MattDolan <matt.r.dolan@gmail.com>
2016-03-15 07:07:04 -0600

    Write to an XLSX file without Excel installed

---
+++ WriteToXLSX/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WriteToXLSX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(304, 256);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(64, 32);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Arguments :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(275, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "1. The .XLSX file to be written to.  Full path and filename.";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(16, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(344, 40);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "2. The file that holds the data to write to the spreadsheet.  Full path and filen" +
+    "ame.  The data should be one line per cell.  The cell name. pipe, cell data. Exa" +
+    "mple A1|Some Value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 300);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Write To .XLSX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WriteToXLSX/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using OfficeOpenXml;         // This is for EPPlus - Creating spreadsheets
+using OfficeOpenXml.Style;   // This is for EPPlus - Creating spreadsheets
+using ADODB;
+
+namespace WriteToXLSX
+{
+    public partial class Form1 : Form
+    {
+        string strXLSXfile = "";
+        string strDataFile = "";
+        bool bComplete = false;
+        private ADODB.Recordset rsCellData = new ADODB.Recordset();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            /* Get the arguments */
+
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length != 2)
+            {
+                MessageBox.Show("No command line argument provided. " + args[0]);
+                return;
+            }
+
+            /* Break them up since we know the expected format */
+
+            string[] argsSplit = args[1].Split('|');
+            if (argsSplit.Length != 2)
+            {
+                MessageBox.Show("Not all arguments were provided.");
+                return;
+            }
+
+            /* Get the input XLSX file and make sure it is good */
+
+            strXLSXfile = argsSplit[0].Trim();
+            if (strXLSXfile == "")
+            {
+                MessageBox.Show("No input XLSX file provided.");
+                return;
+            }
+            if (File.Exists(strXLSXfile) == false)
+            {
+                MessageBox.Show("Target file provided could not be found." + Environment.NewLine + strXLSXfile);
+                return;
+            }
+
+            /* Get the output file and make sure it is good */
+
+            strDataFile = argsSplit[1].Trim();
+            if (strDataFile == "")
+            {
+                MessageBox.Show("No output XLSX file provided.");
+                return;
+            }
+            if (File.Exists(strDataFile) == false)
+            {
+                MessageBox.Show("Data file provided doesn't exist." + Environment.NewLine + strDataFile);
+                File.Delete(strDataFile);
+            }
+
+            WriteData();
+
+        }
+
+        private void WriteData()
+        {
+
+            /* Read up the data to be written to the spreadsheet from the text file */
+            
+            rsCellData.Fields.Append("Cell", DataTypeEnum.adVarChar, 10);
+            rsCellData.Fields.Append("Value", DataTypeEnum.adVarChar, 512);
+            rsCellData.Open();
+
+            string line;
+            int counter = 0;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(strDataFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == "")
+                {
+                    MessageBox.Show("Bad data format at line " + counter.ToString() + " No data on this line. Exiting.");
+                    return;
+                }
+                counter++;
+
+                int iIndex = line.IndexOf('|');
+                if (iIndex == -1)
+                {
+                    MessageBox.Show("Bad data format at line " + counter.ToString() + " No Pipe delimiter found in line. Exiting.");
+                    return;
+                }
+
+                rsCellData.AddNew();
+                rsCellData.Fields["Cell"].Value = line.Substring(0, iIndex);
+                iIndex++;
+                rsCellData.Fields["Value"].Value = line.Substring(iIndex, line.Length - iIndex);
+                rsCellData.Update();
+            }
+
+            if (rsCellData.RecordCount > 0)
+            {
+                rsCellData.MoveFirst();
+            }
+
+            /* Open and write the XlSX file. */
+            
+            FileInfo TargetFile = new FileInfo(strXLSXfile);
+            
+            try
+            {
+                using (ExcelPackage package = new ExcelPackage(TargetFile))
+                {
+
+                    /* Get the work book in the file */
+
+                    ExcelWorkbook workBook = package.Workbook;
+                    if (workBook != null)
+                    {
+                        if (workBook.Worksheets.Count < 0)
+                        {
+                            MessageBox.Show("There are no worksheets in the workbook.");
+                            package.Dispose();
+                            workBook.Dispose();
+                            return;
+                        }
+
+                        /* Get the first worksheet */
+
+                        //ExcelWorksheet Worksheet = workBook.Worksheets.First();
+                        var worksheet = package.Workbook.Worksheets[1];
+
+                        /* Loop throug the recordset and write the data to the worksheet */
+                        string strCell = "";
+                        for (; !rsCellData.EOF; rsCellData.MoveNext())
+                        {
+                            strCell = rsCellData.Fields["Cell"].Value;
+                            worksheet.Cells[strCell].Value = rsCellData.Fields["Value"].Value;
+                        }
+                        worksheet.Cells["C13:C47"].Style.WrapText = true;
+                    }
+                    /* Save the workbook */
+                    try
+                    {
+                        package.Save();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error saving the ATT West North Regional JPA Invoice report." + Environment.NewLine + ex);
+                        return;
+                    }
+                    package.Dispose();
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Error opening spreadsheet. Is it already open? Close it and try again." + Environment.NewLine + Ex.Message);
+                return;
+            }
+            rsCellData.Close();
+            bComplete = true;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (bComplete == true)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
